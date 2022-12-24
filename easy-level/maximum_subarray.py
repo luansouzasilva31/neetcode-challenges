36636227b042a715d27d69a6c878b64f2d27c759
Luan de Souza Silva <luan.silva@grupoicts.com.br>
2022-12-24 16:15:59 -0300

    Create second version of maximum subarray

--- easy-level/maximum_subarray.py
+++ easy-level/maximum_subarray.py
@@ -24,9 +24,37 @@ def get_max_sum_v1(nums:list) -> int:
     
     return max_sum
 
+def get_max_sum_v2(nums:list) -> int:
+    # Based on NeetCode theoretical explanation.
+    
+    n = len(nums)
+    cur_sum = nums[0] # reference
+    max_sum = nums[0]
+    max_val = nums[0]
+    
+    for i in range(1, n):
+        if nums[i] > max_val:
+            max_val = nums[i]
+        
+        if cur_sum <= 0:
+            cur_sum = nums[i]
+            continue
+        
+        max_sum = cur_sum if cur_sum > max_sum else max_sum
+        cur_sum += nums[i]
+        
+    max_sum = cur_sum if cur_sum > max_sum else max_sum
+    max_sum = max_sum if max_sum > max_val else max_val
+    
+    return max_sum
+
 if __name__ == '__main__':
     nums = [-2, 1, -3, 4, -1, 2, 1, -5, 114]
     
     start_time = time.time()
     print(get_max_sum_v1(nums))
+    print(f'----- {(time.time() - start_time) * 1000} ms -----')
+    
+    start_time = time.time()
+    print(get_max_sum_v2(nums))
     print(f'----- {(time.time() - start_time) * 1000} ms -----')
